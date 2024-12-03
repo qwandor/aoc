b77e8c637ea8fe7012f32d1b517dfcc9bda00f28
Andrew Walbran <qwandor@google.com>
2024-12-03 21:22:34 +0000

    Day 1 part 1.

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::{OptionExt, Report};
+use std::io::stdin;
+
+fn main() -> Result<(), Report> {
+    // Read lists from stdin, parsing and splitting into two lists.
+    let (mut left, mut right) = stdin()
+        .lines()
+        .map(|line| {
+            let line = line?;
+            let (left, right) = line.split_once(' ').ok_or_eyre("Missing delimiter")?;
+            Ok((left.parse::<u64>()?, right.trim().parse::<u64>()?))
+        })
+        .collect::<Result<(Vec<_>, Vec<_>), Report>>()?;
+
+    // Sort them both.
+    left.sort();
+    right.sort();
+
+    // Calculate and sum the distances.
+    let total_distance = left
+        .into_iter()
+        .zip(right.into_iter())
+        .map(|(left, right)| left.abs_diff(right))
+        .sum::<u64>();
+
+    println!("Total distance: {}", total_distance);
+
+    Ok(())
 }
