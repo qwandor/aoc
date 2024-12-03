8d3385a83dc986a75f9ee780fd6eb9d4659e3aa7
Andrew Walbran <qwandor@google.com>
2024-12-03 21:28:18 +0000

    Move distance calculation to a function and add a unit test.

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -16,14 +16,28 @@ fn main() -> Result<(), Report> {
     left.sort();
     right.sort();
 
-    // Calculate and sum the distances.
-    let total_distance = left
-        .into_iter()
-        .zip(right.into_iter())
-        .map(|(left, right)| left.abs_diff(right))
-        .sum::<u64>();
+    let total_distance = total_distance(&left, &right);
 
     println!("Total distance: {}", total_distance);
 
     Ok(())
+}
+
+/// Calculates and sums the distances.
+fn total_distance(left_sorted: &[u64], right_sorted: &[u64]) -> u64 {
+    left_sorted
+        .into_iter()
+        .zip(right_sorted.into_iter())
+        .map(|(left, right)| left.abs_diff(*right))
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_distance() {
+        assert_eq!(total_distance(&[1, 2, 3, 3, 3, 4], &[3, 3, 3, 4, 5, 9]), 11);
+    }
 }
