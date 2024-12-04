104fc8dfffef611eafc219ee544341cbf0aeeea0
Andrew Walbran <qwandor@google.com>
2024-12-04 10:21:51 +0000

    Day 2 part 1.

---
+++ 2024/day2/src/main.rs
@@ -0,0 +1,65 @@
+use eyre::Report;
+use std::{cmp::Ordering, io::stdin, ops::RangeInclusive};
+
+const SAFE_CHANGE_RANGE: RangeInclusive<u64> = 1..=3;
+
+fn main() -> Result<(), Report> {
+    let reports: Vec<Vec<u64>> = stdin()
+        .lines()
+        .map(|line| {
+            let line = line?;
+            line.split_whitespace()
+                .map(|level| Ok(level.parse()?))
+                .collect()
+        })
+        .collect::<Result<_, Report>>()?;
+
+    let safe_count = count_safe(&reports);
+    println!("{} reports are safe.", safe_count);
+
+    Ok(())
+}
+
+fn count_safe(reports: &[Vec<u64>]) -> usize {
+    reports.into_iter().filter(|report| safe(report)).count()
+}
+
+/// Returns whether the levels are either all increasing or all decreasing, and adjacent levels
+/// differ by `SAFE_CHANGE_RANGE`.
+fn safe(levels: &[u64]) -> bool {
+    if levels.len() < 2 {
+        return true;
+    };
+    let first_direction = levels[0].cmp(&levels[1]);
+    first_direction != Ordering::Equal
+        && levels.windows(2).all(|window| {
+            let [a, b] = window else { unreachable!() };
+            SAFE_CHANGE_RANGE.contains(&a.abs_diff(*b)) && a.cmp(b) == first_direction
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn safety_example() {
+        assert!(safe(&[7, 6, 4, 2, 1]));
+        assert!(!safe(&[1, 2, 7, 8, 9]));
+        assert!(!safe(&[9, 7, 6, 2, 1]));
+        assert!(!safe(&[1, 3, 2, 4, 5]));
+        assert!(!safe(&[8, 6, 4, 4, 1]));
+        assert!(safe(&[1, 3, 6, 7, 9]));
+    }
+
+    #[test]
+    fn safety_empty() {
+        assert!(safe(&[]));
+    }
+
+    #[test]
+    fn safety_single() {
+        assert!(safe(&[0]));
+        assert!(safe(&[1]));
+    }
+}
