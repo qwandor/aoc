616c4cec8da7dd39afc2367d931c0fea9889de1e
Andrew Walbran <qwandor@google.com>
2024-12-09 19:17:38 +0000

    Day 8 part 2.

--- 2024/day8/src/main.rs
+++ 2024/day8/src/main.rs
@@ -2,13 +2,18 @@ use eyre::Report;
 use std::{
     collections::{HashMap, HashSet},
     io::{stdin, BufRead},
+    ops::{Add, Mul, Sub},
 };
 use utils::parse_chargrid;
 
 fn main() -> Result<(), Report> {
     let world = parse(stdin().lock())?;
-    let antinode_count = count_antinodes(&world);
-    println!("{} antinodes.", antinode_count);
+
+    let original_antinode_count = count_antinodes(&world, original_antinodes_for_antennas);
+    println!("{} antinodes by original model.", original_antinode_count);
+
+    let expanded_antinode_count = count_antinodes(&world, expanded_antinodes_for_antennas);
+    println!("{} antinodes by expanded model.", expanded_antinode_count);
 
     Ok(())
 }
@@ -39,7 +44,10 @@ fn parse(input: impl BufRead) -> Result<World, Report> {
     })
 }
 
-fn count_antinodes(world: &World) -> usize {
+fn count_antinodes(
+    world: &World,
+    antinodes_for_antennas: fn((usize, usize), (usize, usize), usize, usize) -> Vec<(usize, usize)>,
+) -> usize {
     let mut antennas_by_frequency: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
     for antenna in &world.antennas {
         antennas_by_frequency
@@ -56,7 +64,7 @@ fn count_antinodes(world: &World) -> usize {
                 .enumerate()
                 .flat_map(|(i, antenna1)| {
                     antenna_positions[0..i].iter().flat_map(|antenna2| {
-                        antinodes_for_antennas(antenna1, antenna2, world.width, world.height)
+                        antinodes_for_antennas(*antenna1, *antenna2, world.width, world.height)
                     })
                 })
         })
@@ -65,9 +73,9 @@ fn count_antinodes(world: &World) -> usize {
     antinodes.len()
 }
 
-fn antinodes_for_antennas(
-    antenna1: &(usize, usize),
-    antenna2: &(usize, usize),
+fn original_antinodes_for_antennas(
+    antenna1: (usize, usize),
+    antenna2: (usize, usize),
     width: usize,
     height: usize,
 ) -> Vec<(usize, usize)> {
@@ -89,6 +97,62 @@ fn antinodes_for_antennas(
         }
     }
     antinodes
+}
+
+fn expanded_antinodes_for_antennas(
+    antenna1: (usize, usize),
+    antenna2: (usize, usize),
+    width: usize,
+    height: usize,
+) -> Vec<(usize, usize)> {
+    let antenna1 = IVec2D(antenna1.0 as isize, antenna1.1 as isize);
+    let antenna2 = IVec2D(antenna2.0 as isize, antenna2.1 as isize);
+    let width = width as isize;
+    let height = height as isize;
+    let difference = antenna2 - antenna1;
+    let mut antinodes = Vec::new();
+    for i in 0.. {
+        let antinode = antenna1 + difference * i;
+        if antinode.0 < 0 || antinode.1 < 0 || antinode.0 >= width || antinode.1 >= height {
+            break;
+        }
+        antinodes.push((antinode.0 as usize, antinode.1 as usize));
+    }
+    for i in 1.. {
+        let antinode = antenna1 - difference * i;
+        if antinode.0 < 0 || antinode.1 < 0 || antinode.0 >= width || antinode.1 >= height {
+            break;
+        }
+        antinodes.push((antinode.0 as usize, antinode.1 as usize));
+    }
+    antinodes
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct IVec2D(isize, isize);
+
+impl Add for IVec2D {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Sub for IVec2D {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Mul<isize> for IVec2D {
+    type Output = Self;
+
+    fn mul(self, rhs: isize) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -187,6 +251,19 @@ mod tests {
             .as_bytes(),
         )
         .unwrap();
-        assert_eq!(count_antinodes(&world), 14);
+        assert_eq!(count_antinodes(&world, original_antinodes_for_antennas), 14);
+        assert_eq!(count_antinodes(&world, expanded_antinodes_for_antennas), 34);
+    }
+
+    #[test]
+    fn expanded_antinodes_pair() {
+        assert_eq!(
+            expanded_antinodes_for_antennas((2, 2), (3, 4), 10, 10),
+            vec![(2, 2), (3, 4), (4, 6), (5, 8), (1, 0)]
+        );
+        assert_eq!(
+            expanded_antinodes_for_antennas((4, 2), (3, 4), 10, 10),
+            vec![(4, 2), (3, 4), (2, 6), (1, 8), (5, 0)]
+        );
     }
 }
