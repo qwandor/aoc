c583536289784713f1809905ab1e277f1289b903
Andrew Walbran <qwandor@google.com>
2024-12-11 10:43:48 +0000

    Day 11 part 2.

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,34 +1,47 @@
 use eyre::Report;
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
 fn main() -> Result<(), Report> {
     let mut line = String::new();
     stdin().read_line(&mut line)?;
-    let mut stones = line
+    let stones = line
         .split_whitespace()
         .map(|stone| Ok(stone.parse()?))
         .collect::<Result<Vec<u64>, Report>>()?;
+    let mut stone_counts = count(&stones);
     for i in 1..=75 {
-        stones = blink(&stones);
-        println!("{} stones after blinking {i} times.", stones.len());
+        stone_counts = blink(&stone_counts);
+        println!(
+            "{} stones ({} distinct) after blinking {i} times.",
+            stone_counts.values().sum::<usize>(),
+            stone_counts.len(),
+        );
     }
 
     Ok(())
 }
 
-fn blink(stones: &[u64]) -> Vec<u64> {
-    stones
-        .iter()
-        .flat_map(|stone| {
-            if *stone == 0 {
-                vec![1]
-            } else if let Some((a, b)) = split(*stone) {
-                vec![a, b]
-            } else {
-                vec![stone * 2024]
-            }
-        })
-        .collect()
+fn count(stones: &[u64]) -> HashMap<u64, usize> {
+    let mut counts = HashMap::new();
+    for stone in stones {
+        *counts.entry(*stone).or_default() += 1;
+    }
+    counts
+}
+
+fn blink(stone_counts: &HashMap<u64, usize>) -> HashMap<u64, usize> {
+    let mut new_counts = HashMap::new();
+    for (stone, count) in stone_counts {
+        if *stone == 0 {
+            *new_counts.entry(1).or_default() += count;
+        } else if let Some((a, b)) = split(*stone) {
+            *new_counts.entry(a).or_default() += count;
+            *new_counts.entry(b).or_default() += count;
+        } else {
+            *new_counts.entry(stone * 2024).or_default() += count;
+        }
+    }
+    new_counts
 }
 
 fn split(stone: u64) -> Option<(u64, u64)> {
@@ -50,15 +63,16 @@ mod tests {
     #[test]
     fn blink_example() {
         let stones = vec![125, 17];
-        let stones = blink(&stones);
-        assert_eq!(stones, vec![253000, 1, 7]);
-        let stones = blink(&stones);
-        assert_eq!(stones, vec![253, 0, 2024, 14168]);
-        let stones = blink(&stones);
-        assert_eq!(stones, vec![512072, 1, 20, 24, 28676032]);
-        let stones = blink(&stones);
-        let stones = blink(&stones);
-        let stones = blink(&stones);
-        assert_eq!(stones.len(), 22);
+        let stone_counts = count(&stones);
+        let stone_counts = blink(&stone_counts);
+        assert_eq!(stone_counts, count(&[253000, 1, 7]));
+        let stone_counts = blink(&stone_counts);
+        assert_eq!(stone_counts, count(&[253, 0, 2024, 14168]));
+        let stone_counts = blink(&stone_counts);
+        assert_eq!(stone_counts, count(&[512072, 1, 20, 24, 28676032]));
+        let stone_counts = blink(&stone_counts);
+        let stone_counts = blink(&stone_counts);
+        let stone_counts = blink(&stone_counts);
+        assert_eq!(stone_counts.values().sum::<usize>(), 22);
     }
 }
