d0923876848753dc8dcc1c3ef0b527e34592cdca
Andrew Walbran <qwandor@google.com>
2024-12-23 16:41:39 +0000

    Day 22 part 2 (too slow).

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -9,10 +9,22 @@ fn main() -> Result<(), Report> {
         .collect::<Result<Vec<u64>, Report>>()?;
 
     let sum_2000th = buyer_initial_numbers
-        .into_iter()
-        .map(|initial| iterate(next_secret_number, initial, 2000))
+        .iter()
+        .map(|initial| {
+            SecretNumberIterator { next: *initial }
+                .skip(2000)
+                .next()
+                .unwrap()
+        })
         .sum::<u64>();
     println!("Sum of 2000th secret numbers: {}", sum_2000th);
+
+    let best_sequence = find_best_sequence(&buyer_initial_numbers);
+    let best_sequence_profit = total_profit_for_sequence(&buyer_initial_numbers, &best_sequence);
+    println!(
+        "Best sequence is {:?}, for {} bananas",
+        best_sequence, best_sequence_profit
+    );
 
     Ok(())
 }
@@ -23,12 +35,78 @@ fn next_secret_number(number: u64) -> u64 {
     (b ^ (b * 2048)) % 16777216
 }
 
-fn iterate<T>(f: impl Fn(T) -> T, initial: T, iterations: usize) -> T {
-    let mut value = initial;
-    for _ in 0..iterations {
-        value = f(value);
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct SecretNumberIterator {
+    next: u64,
+}
+
+impl Iterator for SecretNumberIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let number = self.next;
+        self.next = next_secret_number(self.next);
+        Some(number)
     }
-    value
+}
+
+fn changes(prices: &[u64]) -> Vec<i64> {
+    prices
+        .windows(2)
+        .map(|prices| (prices[1] as i64) - (prices[0] as i64))
+        .collect()
+}
+
+fn profit_for_sequence(initial_number: u64, sequence: &[i64; 4]) -> u64 {
+    let prices = SecretNumberIterator {
+        next: initial_number,
+    }
+    .take(2001)
+    .map(|number| number % 10)
+    .collect::<Vec<_>>();
+    prices
+        .windows(5)
+        .filter_map(|prices| {
+            if changes(prices) == sequence {
+                Some(*prices.last().unwrap())
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap_or_default()
+}
+
+/// Returns the total number of bananas that would be gained by giving the negotating monkey the
+/// given sequence of changes.
+fn total_profit_for_sequence(initial_numbers: &[u64], sequence: &[i64; 4]) -> u64 {
+    initial_numbers
+        .iter()
+        .map(|initial_number| profit_for_sequence(*initial_number, sequence))
+        .sum()
+}
+
+/// Finds the best sequence of changes to tell the monkey for the given set of initial buyer secret
+/// numbers.
+fn find_best_sequence(initial_numbers: &[u64]) -> [i64; 4] {
+    let mut best_sequence = [0; 4];
+    let mut best_profit = 0;
+    for a in -9..9 {
+        for b in -9..9 {
+            println!("Trying {}, {}, ...", a, b);
+            for c in -9..9 {
+                println!("Trying {}, {}, {}, ...", a, b, c);
+                for d in -9..9 {
+                    let profit = total_profit_for_sequence(initial_numbers, &[a, b, c, d]);
+                    if profit > best_profit {
+                        best_sequence = [a, b, c, d];
+                        best_profit = profit;
+                    }
+                }
+            }
+        }
+    }
+    best_sequence
 }
 
 #[cfg(test)]
@@ -51,9 +129,41 @@ mod tests {
 
     #[test]
     fn iterate_example() {
-        assert_eq!(iterate(next_secret_number, 1, 2000), 8685429);
-        assert_eq!(iterate(next_secret_number, 10, 2000), 4700978);
-        assert_eq!(iterate(next_secret_number, 100, 2000), 15273692);
-        assert_eq!(iterate(next_secret_number, 2024, 2000), 8667524);
+        assert_eq!(
+            SecretNumberIterator { next: 1 }.skip(2000).next().unwrap(),
+            8685429
+        );
+        assert_eq!(
+            SecretNumberIterator { next: 10 }.skip(2000).next().unwrap(),
+            4700978
+        );
+        assert_eq!(
+            SecretNumberIterator { next: 100 }
+                .skip(2000)
+                .next()
+                .unwrap(),
+            15273692
+        );
+        assert_eq!(
+            SecretNumberIterator { next: 2024 }
+                .skip(2000)
+                .next()
+                .unwrap(),
+            8667524
+        );
+    }
+
+    #[test]
+    fn example_sequence_profit() {
+        assert_eq!(profit_for_sequence(1, &[-2, 1, -1, 3]), 7);
+        assert_eq!(profit_for_sequence(2, &[-2, 1, -1, 3]), 7);
+        assert_eq!(profit_for_sequence(3, &[-2, 1, -1, 3]), 0);
+        assert_eq!(profit_for_sequence(2024, &[-2, 1, -1, 3]), 9);
+    }
+
+    #[test]
+    #[ignore] // Long test, so skip by default.
+    fn best_sequence_example() {
+        assert_eq!(find_best_sequence(&[1, 2, 3, 2024]), [-2, 1, -1, 3]);
     }
 }
