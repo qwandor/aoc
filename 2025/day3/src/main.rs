8b522f8e45723b0854404906cfbba69e187722ac
Andrew Walbran <qwandor@google.com>
2025-12-03 11:52:30 +0000

    Day 3 part 2.

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -4,7 +4,14 @@ use std::io::{BufRead, stdin};
 fn main() -> Result<(), Report> {
     let banks = parse(stdin().lock())?;
 
-    println!("Max total joltage: {}", total_max_joltage(&banks));
+    println!(
+        "Max total joltage with 2 cells: {}",
+        total_max_joltage(&banks, 2)
+    );
+    println!(
+        "Max total joltage with 12 cells: {}",
+        total_max_joltage(&banks, 12)
+    );
 
     Ok(())
 }
@@ -21,19 +28,25 @@ fn parse(input: impl BufRead) -> Result<Vec<Vec<u32>>, Report> {
         .collect()
 }
 
-fn total_max_joltage(banks: &[Vec<u32>]) -> u32 {
-    banks.iter().map(|bank| max_joltage(bank)).sum()
+fn total_max_joltage(banks: &[Vec<u32>], cell_count: usize) -> u64 {
+    banks.iter().map(|bank| max_joltage(bank, cell_count)).sum()
 }
 
 /// Returns the maximum joltage for a single bank.
-fn max_joltage(bank: &[u32]) -> u32 {
-    if bank.len() < 2 {
+fn max_joltage(mut bank: &[u32], cell_count: usize) -> u64 {
+    if bank.len() < cell_count {
         return 0;
     }
-    // The first index can't be the last value, as there must be space left for the second index.
-    let first_index = first_max_index(&bank[0..bank.len() - 1]).unwrap();
-    let second_index = first_max_index(&bank[first_index + 1..]).unwrap() + first_index + 1;
-    bank[first_index] * 10 + bank[second_index]
+
+    let mut total = 0;
+    for remaining_digits in (0..cell_count).rev() {
+        // Leave space for the remaining digits.
+        let next_index = first_max_index(&bank[0..bank.len() - remaining_digits]).unwrap();
+        total = total * 10 + u64::from(bank[next_index]);
+        bank = &bank[next_index + 1..];
+    }
+
+    total
 }
 
 /// Returns the index of the first value which is equal to the maximum.
@@ -70,35 +83,74 @@ mod tests {
     }
 
     #[test]
-    fn example_max_joltage() {
+    fn example_max_joltage_2() {
         assert_eq!(
-            max_joltage(&[9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]),
+            max_joltage(&[9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2),
             98
         );
         assert_eq!(
-            max_joltage(&[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]),
+            max_joltage(&[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 2),
             89
         );
         assert_eq!(
-            max_joltage(&[2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8]),
+            max_joltage(&[2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 2),
             78
         );
         assert_eq!(
-            max_joltage(&[8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1]),
+            max_joltage(&[8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 2),
             92
         );
     }
 
     #[test]
-    fn example_max_total_joltage() {
+    fn example_max_total_joltage_2() {
         assert_eq!(
-            total_max_joltage(&[
-                vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
-                vec![8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
-                vec![2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
-                vec![8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
-            ]),
+            total_max_joltage(
+                &[
+                    vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+                    vec![8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+                    vec![2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+                    vec![8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
+                ],
+                2
+            ),
             357
+        );
+    }
+
+    #[test]
+    fn example_max_joltage_12() {
+        assert_eq!(
+            max_joltage(&[9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 12),
+            987654321111
+        );
+        assert_eq!(
+            max_joltage(&[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 12),
+            811111111119
+        );
+        assert_eq!(
+            max_joltage(&[2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 12),
+            434234234278
+        );
+        assert_eq!(
+            max_joltage(&[8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 12),
+            888911112111
+        );
+    }
+
+    #[test]
+    fn example_max_total_joltage_12() {
+        assert_eq!(
+            total_max_joltage(
+                &[
+                    vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+                    vec![8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+                    vec![2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+                    vec![8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
+                ],
+                12
+            ),
+            3121910778619
         );
     }
 }
