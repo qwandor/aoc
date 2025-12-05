5427a34c14f7250c9493944a914043791fcdfb97
Andrew Walbran <qwandor@google.com>
2025-12-05 12:00:02 +0000

    Day 5 part 2.

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -8,6 +8,7 @@ fn main() -> Result<(), Report> {
     let ingredients = parse(stdin().lock())?;
 
     println!("Fresh ingredient count: {}", ingredients.fresh_count());
+    println!("Total fresh IDs: {}", ingredients.fresh_ranges_size());
 
     Ok(())
 }
@@ -30,6 +31,39 @@ impl Ingredients {
             })
             .count()
     }
+
+    /// Returns the total number of ingredients covered by the fresh ranges.
+    fn fresh_ranges_size(&self) -> u64 {
+        let merged_ranges = merge_ranges(&self.fresh_ranges);
+        println!(
+            "Merged {} ranges into {} ranges",
+            self.fresh_ranges.len(),
+            merged_ranges.len()
+        );
+        merged_ranges
+            .into_iter()
+            .map(|range| range.end() - range.start() + 1)
+            .sum()
+    }
+}
+
+fn merge_ranges(ranges: &[RangeInclusive<u64>]) -> Vec<RangeInclusive<u64>> {
+    let mut sorted = ranges.to_owned();
+    sorted.sort_by_key(|range| *range.start());
+
+    let mut merged = Vec::<RangeInclusive<u64>>::new();
+    for range in sorted {
+        if let Some(last) = merged.last_mut()
+            && last.contains(range.start())
+        {
+            if !last.contains(range.end()) {
+                *last = *last.start()..=*range.end()
+            }
+        } else {
+            merged.push(range)
+        }
+    }
+    merged
 }
 
 fn parse_range(line: &str) -> Result<RangeInclusive<u64>, Report> {
@@ -103,5 +137,21 @@ mod tests {
             available_ingredients: vec![1, 5, 8, 11, 17, 32],
         };
         assert_eq!(ingredients.fresh_count(), 3);
+    }
+
+    #[test]
+    fn fresh_range_size_example() {
+        let ingredients = Ingredients {
+            fresh_ranges: vec![3..=5, 10..=14, 16..=20, 12..=18],
+            available_ingredients: vec![1, 5, 8, 11, 17, 32],
+        };
+        assert_eq!(ingredients.fresh_ranges_size(), 14);
+    }
+
+    #[test]
+    fn merge_more() {
+        assert_eq!(merge_ranges(&[]), vec![]);
+        assert_eq!(merge_ranges(&[0..=1, 1..=2]), vec![0..=2]);
+        assert_eq!(merge_ranges(&[1..=5, 2..=3]), vec![1..=5]);
     }
 }
