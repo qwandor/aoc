d8d08b4e6cb17c72fb44cfe383d8378e21bf1bf9
Andrew Walbran <qwandor@google.com>
2025-12-08 01:23:05 +0000

    Combine both parts into a single function.

--- 2025/day7/src/main.rs
+++ 2025/day7/src/main.rs
@@ -4,48 +4,14 @@ use utils::{grid::Grid, parse_chargrid};
 
 fn main() -> Result<(), Report> {
     let grid = parse_chargrid(stdin().lock())?;
-    println!("Beam splits: {}", count_splits(&grid)?);
-    println!("Timelines: {}", count_timelines(&grid)?);
+    let (splits, timelines) = count_splits_and_timelines(&grid)?;
+    println!("Beam splits: {splits}");
+    println!("Timelines: {timelines}");
 
     Ok(())
 }
 
-fn count_splits(grid: &Grid<char>) -> Result<usize, Report> {
-    let mut beams = grid
-        .rows()
-        .next()
-        .ok_or_eyre("Empty grid")?
-        .iter()
-        .map(|&c| c == 'S')
-        .collect::<Vec<_>>();
-
-    let mut split_count = 0;
-
-    let width = grid.width();
-    for row in grid.rows().skip(1) {
-        let mut new_beams = vec![false; width];
-        for (x, (&beam, &c)) in beams.iter().zip(row.iter()).enumerate() {
-            if beam {
-                if c == '^' {
-                    split_count += 1;
-                    if x > 0 {
-                        new_beams[x - 1] = true;
-                    }
-                    if x < width - 1 {
-                        new_beams[x + 1] = true;
-                    }
-                } else {
-                    new_beams[x] = true;
-                }
-            }
-        }
-        beams = new_beams;
-    }
-
-    Ok(split_count)
-}
-
-fn count_timelines(grid: &Grid<char>) -> Result<u64, Report> {
+fn count_splits_and_timelines(grid: &Grid<char>) -> Result<(usize, u64), Report> {
     let mut beams = grid
         .rows()
         .next()
@@ -54,11 +20,15 @@ fn count_timelines(grid: &Grid<char>) -> Result<u64, Report> {
         .map(|&c| if c == 'S' { 1 } else { 0 })
         .collect::<Vec<u64>>();
 
+    let mut split_count = 0;
     let width = grid.width();
     for row in grid.rows().skip(1) {
         let mut new_beams = vec![0; width];
         for (x, (&beam, &c)) in beams.iter().zip(row.iter()).enumerate() {
             if c == '^' {
+                if beam > 0 {
+                    split_count += 1;
+                }
                 if x > 0 {
                     new_beams[x - 1] += beam;
                 }
@@ -73,7 +43,7 @@ fn count_timelines(grid: &Grid<char>) -> Result<u64, Report> {
         beams = new_beams;
     }
 
-    Ok(beams.iter().sum())
+    Ok((split_count, beams.iter().sum()))
 }
 
 #[cfg(test)]
@@ -105,7 +75,6 @@ mod tests {
         )
         .unwrap();
 
-        assert_eq!(count_splits(&grid).unwrap(), 21);
-        assert_eq!(count_timelines(&grid).unwrap(), 40);
+        assert_eq!(count_splits_and_timelines(&grid).unwrap(), (21, 40));
     }
 }
