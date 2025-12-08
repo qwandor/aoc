818e386108c6f26c5d1597eb392a6c5f6b1c65e1
Andrew Walbran <qwandor@google.com>
2025-12-08 01:20:57 +0000

    Day 7 part 2.

--- 2025/day7/src/main.rs
+++ 2025/day7/src/main.rs
@@ -5,6 +5,7 @@ use utils::{grid::Grid, parse_chargrid};
 fn main() -> Result<(), Report> {
     let grid = parse_chargrid(stdin().lock())?;
     println!("Beam splits: {}", count_splits(&grid)?);
+    println!("Timelines: {}", count_timelines(&grid)?);
 
     Ok(())
 }
@@ -44,6 +45,37 @@ fn count_splits(grid: &Grid<char>) -> Result<usize, Report> {
     Ok(split_count)
 }
 
+fn count_timelines(grid: &Grid<char>) -> Result<u64, Report> {
+    let mut beams = grid
+        .rows()
+        .next()
+        .ok_or_eyre("Empty grid")?
+        .iter()
+        .map(|&c| if c == 'S' { 1 } else { 0 })
+        .collect::<Vec<u64>>();
+
+    let width = grid.width();
+    for row in grid.rows().skip(1) {
+        let mut new_beams = vec![0; width];
+        for (x, (&beam, &c)) in beams.iter().zip(row.iter()).enumerate() {
+            if c == '^' {
+                if x > 0 {
+                    new_beams[x - 1] += beam;
+                }
+                if x < width - 1 {
+                    new_beams[x + 1] += beam;
+                }
+            } else {
+                new_beams[x] += beam;
+            }
+        }
+
+        beams = new_beams;
+    }
+
+    Ok(beams.iter().sum())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,5 +106,6 @@ mod tests {
         .unwrap();
 
         assert_eq!(count_splits(&grid).unwrap(), 21);
+        assert_eq!(count_timelines(&grid).unwrap(), 40);
     }
 }
